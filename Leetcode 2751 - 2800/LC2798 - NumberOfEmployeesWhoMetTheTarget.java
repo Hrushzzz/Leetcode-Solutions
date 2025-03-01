16bf45949d8cb2b57fedd528c776a8c74d0f3d29
SpiderMan <hrushikesh.tk@gmail.com>
2025-03-01 21:56:17 +0530

    push

---
+++ Leetcode 2751 - 2800/LC2798 - NumberOfEmployeesWhoMetTheTarget.java
@@ -0,0 +1,13 @@
+// Problem Link ::: https://leetcode.com/problems/number-of-employees-who-met-the-target/description/
+
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int count = 0;
+        for (int i = 0; i < hours.length; i++) {
+            if (hours[i] >= target) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
