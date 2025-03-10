661a4db7b4aa79e2a93be2b0aad0aea756a43ce6
SpiderMan <hrushikesh.tk@gmail.com>
2025-03-10 23:16:41 +0530

    push

---
+++ Leetcode 2650 - 2700/LC2667 - CreateHelloWorldFunction.js
@@ -0,0 +1,7 @@
+// Problem Link ::: https://leetcode.com/problems/create-hello-world-function/description/
+
+var createHelloWorld = function() {
+    return function() {
+        return "Hello World";
+    }
+};
