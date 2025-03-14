14d0921fadcea8bcd28273925ea7d7e5c8225778
SpiderMan <hrushikesh.tk@gmail.com>
2025-03-14 22:13:41 +0530

    push

--- Leetcode 0151 - 0200/LC0185 - DepartmentTopThreeSalaries.sql
+++ Leetcode 0151 - 0200/LC0185 - DepartmentTopThreeSalaries.sql
@@ -1,1 +1,17 @@
-Hello World..!
+-- Problem Link ::: https://leetcode.com/problems/department-top-three-salaries/description/
+
+SELECT
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM
+    Employee e
+    JOIN Department d ON e.departmentId = d.id
+WHERE
+    (
+        SELECT COUNT(DISTINCT salary)
+        FROM Employee e2
+        WHERE e2.departmentId = e.departmentId AND e2.salary >= e.salary
+    ) <= 3
+ORDER BY
+    Department, Salary DESC;
