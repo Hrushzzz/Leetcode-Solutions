dc49215edec8806166c707f5b309469e25c157de
SpiderMan <hrushikesh.tk@gmail.com>
2026-01-01 23:58:32 +0530

    push

---
+++ Leetcode 0051 - 0100/LC0066 - PlusOne.java
@@ -0,0 +1,17 @@
+Problem Link ::: https://leetcode.com/problems/plus-one/description/
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int N = digits.length;
+        for (int i = N - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] ans = new int[N + 1];
+        ans[0] = 1;
+        return ans;
+    }
+}
