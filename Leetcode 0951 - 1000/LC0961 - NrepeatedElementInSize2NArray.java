a19180c1174ce074a5d3befe47904a5e10aa1faf
SpiderMan <hrushikesh.tk@gmail.com>
2026-01-02 22:51:19 +0530

    push

---
+++ Leetcode 0951 - 1000/LC0961 - NrepeatedElementInSize2NArray.java
@@ -0,0 +1,14 @@
+// Problem Link :: https://leetcode.com/problems/n-repeated-element-in-size-2n-array/description/
+
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(set.contains(nums[i])) {
+                return nums[i];
+            }
+            set.add(nums[i]);
+        }
+        return -1;
+    }
+}
